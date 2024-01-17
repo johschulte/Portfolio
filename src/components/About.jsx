7dc3e6237e85b0419ebef707b6a2efefe17364ed
John Schulte <johannes-schulte@gmx.de>
2024-01-17 10:32:52 +0100

    fix picture

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import portaitabout from "../assets/portraitaboutzen.png";
+import portaitabout from "../assets/portraitaboutzent.png";
 
 const About = () => {
   return (
