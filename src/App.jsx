642256d3cc4c2c2610f93b960a60c1aa5f6e95e1
John Schulte <johannes-schulte@gmx.de>
2024-01-17 10:56:06 +0100

    update favicon

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { useState, useEffect } from "react";
 import BackToTop from "./components/BackToTop";
+import { useFavicon } from "@uidotdev/usehooks";
 
 function App() {
   const [isBackToTopRendered, setIsBackToTopRendered] = useState(false);
+  const [favicon, setFavicon] = React.useState("./assets/favicon.io");
 
   const checkScroll = () => {
     if (window.scrollY > 700) {
